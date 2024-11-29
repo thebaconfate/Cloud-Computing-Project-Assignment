33a502713f65bda6944350568116c5eb16ef02b4
Gérard Lichtert <62557229+thebaconfate@users.noreply.github.com>
2024-11-30 00:52:05 +0100

    updated matching engine

--- matching-engine/main.js
+++ matching-engine/main.js
@@ -73,164 +73,59 @@ class MatchingEngine {
     this.symbol_order_book = new OrderBook(symbols);
   }
 
-  /**
-   * This function takes and order and try to match it against the order book of the order's symbol.
-   * The execution handler is a function that will process the two set of executions if the order is matched.
-   * @param {EngineOrder} order - the new order that has to be executed
-   * @param {Function} executionHandler the callback function that is called once an order has been executed
-   */
   execute(order = new EngineOrder(), executionHandler) {
-    let current_book = this.symbol_order_book.symbol_order_book_map.get(
+    const symbol_book = this.symbol_order_book.symbol_order_book_map.get(
       order.symbol,
     );
 
+    const bids = symbol_book.bids;
+    const asks = symbol_book.asks;
+
+    //first, pushing the incoming order into the book.
     if (order.side === "bid") {
-      // NOTE: BID is a "buy" order
-      // handle bid order
-      if (order.price < current_book.bids.top()?.price_level) {
-        // if the price of the bid order is lower than the highest bid push it into the book to match it later
-        //Adding the order to the book for matching later cause it has a low bid price wrt current biggest bid
-        current_book.bids.push({ price_level: order.price, order: order });
-      } else {
-        //if the price of the order is equal or the higher than the current highest bid, we match the BID
-        //we can match the order cause there are orders cheaper then the bidding price.
-        // const matched_elements = this._match(order, this.symbol_order_book);
-        const matched_elements = this._matchBid(order, current_book.asks);
-        // during the matching, if there are any, the orders will be filled
-        // and the existings stocks will be removed to fill the order, however
-        // if the order was unable to be filled, we get the remaining_qtty to
-        // adapt the order, so that it can be completed once it is able to do so.
-        if (matched_elements.remaining_qtty > 0) {
-          // if there order is not able to be completed, push it for matching later
-          // the quantinty needed by the bidder was insufficient by the asking price volume.
-          order.quantity = matched_elements.remaining_qtty;
-          current_book.bids.push({ price_level: order.price, order: order });
-        }
-        // continuation post matching
-        // TODO: add documentation
-        executionHandler(
-          matched_elements.ask_executions,
-          matched_elements.bid_executions,
-        );
-      }
+      bids.push({ price_level: order.price, order: order });
     } else {
-      // NOTE: ASK is a "sell" order
-      // handle ask order
-      if (order.price > current_book.asks.top()?.price_level) {
-        // if the ask order has a higher price than the current lowest price
-        // Adding the order to the book for matching later cause it has a high ask price wrt current higher ask
-        current_book.asks.push({ price_level: order.price, order: order });
-      } else {
-        // if the ask price is the lowest or equal to the lowest in the book, match the ASK.
-        //we can match the order cause there are order cheaper than the order's bidding price.
-        const matched_elements = this._matchAsk(order, current_book.bids);
-        // const matched_elements = this._match(order, this.symbol_order_book);
-        if (matched_elements.remaining_qtty > 0) {
-          // same things as bid but for ask, try to match the quantity, if
-          // there order was not able to be filled completely, place the
-          // remainder of the order in the book
-          order.quantity = matched_elements.remaining_qtty;
-          current_book.asks.push({ price_level: order.price, order: order });
-        }
-        // continuation post matching
-        // TODO: add documentation of what it expects
-        executionHandler(
-          matched_elements.ask_executions,
-          matched_elements.bid_executions,
-        );
-      }
-    }
-  }
-
-  /*
-   * @param {EngineOrder} bid_order -- the placed bid order
-   * @param {Heap} symbol_asks_heap -- the heap containing all asks
-   */
-  _matchBid(bid_order = new EngineOrder(), symbol_asks_heap = new Heap()) {
-    let bid_amount = bid_order.quantity;
-    let bid_price = bid_order.price;
-    const ask_executions = [];
-    const bid_executions = [];
-    /*
-     * While the bidding price is greater or equal to the lowest ask price
-     * and the order isn't completely filled yet
-     */
-    while (bid_price >= symbol_asks_heap.top()?.price_level && bid_amount > 0) {
-      /*
-       * Get the cheapest ask,
-       */
-      let ask_match = symbol_asks_heap.top();
-      let rem_ask_amount = ask_match.order.quantity - bid_amount;
-      // try to fill the order
-      if (rem_ask_amount >= 0) {
-        // if the ask has remaining stocks update the ask
-        ask_match.order.quantity = rem_ask_amount;
-
-        let ask_match_clone = { ...ask_match.order };
-        // copy the ask order and adapt the remaining quantity
-        ask_match_clone.quantity = bid_amount;
-        // push the updated ask order and the filled bid order to the arrays
-        ask_executions.push(ask_match_clone); //report partial ask match
-        bid_executions.push(bid_order); //report full bid match
-      } else {
-        // if the remaining ask amount is negative, it means the ask order was
-        // filled and has to be popped form the heap as well as pushed to the
-        // ask array
-        ask_executions.push(symbol_asks_heap.pop().order);
-      }
-
-      // the bid order was not filled, so we loop again
-      if (rem_ask_amount < 0) {
-        bid_amount = Math.abs(rem_ask_amount);
-      } else {
-        // early break before testing while condition
-        break;
-      }
-    }
-    // TODO: decipher this
-    if (bid_order.quantity - bid_amount > 0) {
-      let bid_order_clone = { ...bid_order };
-      bid_order_clone.quantity = bid_order.quantity - bid_amount;
-      bid_executions.push(bid_order_clone);
+      asks.push({ price_level: order.price, order: order });
     }
 
-    return { remaining_qtty: bid_amount, ask_executions, bid_executions };
-  }
+    const executed_bids = [];
+    const executed_asks = [];
 
-  _matchAsk(ask_order = new EngineOrder(), symbol_bids_heap = new Heap()) {
-    let ask_amount = ask_order.quantity;
-    const ask_executions = [];
-    const bid_executions = [];
+    //then matching and executing while bid.top <= ask.top
     while (
-      ask_order.price <= symbol_bids_heap.top()?.price_level &&
-      ask_amount > 0
+      bids.top() &&
+      asks.top() &&
+      bids.top().price_level >= asks.top().price_level
     ) {
-      let bid_match = symbol_bids_heap.top();
-      let rem_bid_amount = bid_match.order.quantity - ask_amount;
-      if (rem_bid_amount >= 0) {
-        let bid_match_clone = { ...bid_match.order };
-        bid_match_clone.quantity = ask_amount;
+      const topb = bids.top();
+      const topa = asks.top();
+      //computing the trade quantity for the top bid (highest) and ask(lowest) orders in the book
+      const tradeQtty = Math.min(topb.order.quantity, topa.order.quantity);
 
-        bid_match.order.quantity = rem_bid_amount;
-        bid_executions.push(bid_match_clone); //report full bid match
-        ask_executions.push(ask_order); //report partial ask match
-      } else {
-        bid_executions.push(symbol_bids_heap.pop().order);
+      //performing the trade
+      topb.order.quantity = topb.order.quantity - tradeQtty;
+      topa.order.quantity = topa.order.quantity - tradeQtty;
+
+      //creating the order executions.
+      const bidExec = structuredClone(topb.order);
+      const askExec = structuredClone(topa.order);
+      bidExec.quantity = tradeQtty;
+      askExec.quantity = tradeQtty;
+
+      //collecting the executions on current iteration
+      executed_bids.push(bidExec);
+      executed_asks.push(askExec);
+
+      //the order is removed from the heap whenever its quantity reaches 0
+      if (topb.order.quantity === 0) {
+        bids.pop();
       }
-      if (rem_bid_amount < 0) {
-        ask_amount = Math.abs(rem_bid_amount);
-      } else {
-        break;
+      if (topa.order.quantity === 0) {
+        asks.pop();
       }
     }
 
-    if (ask_order.quantity - ask_amount > 0) {
-      let ask_order_clone = { ...ask_order };
-      ask_order_clone.quantity = ask_order.quantity - ask_amount;
-      ask_executions.push(ask_order_clone);
-    }
-
-    return { remaining_qtty: ask_amount, ask_executions, bid_executions };
+    executionHandler(executed_asks, executed_bids);
   }
 }
 
